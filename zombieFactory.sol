eff330921b76d944ffdfb05fdbb167bf49407db6
Meagan Blais <meaganblais@gmail.com>
2018-03-30 17:39:04 -0400

    initial commit for zombieFactory contract

---
+++ zombieFactory.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+
+  // struct allows for complicated data types that have multiple properties
+  struct Zombie {
+      string name;
+      uint dna;
+  }
+
+  // create Dynamic (non-fixed) array set to public so other contracts can read
+  Zombie[] public zombies;
+
+  // create new Zombie, set parameter variables and add it to the ombies array
+  function createZombie(string _name, uint _dna) {
+    zombies.push(Zombie(_name, _dna));
+  }
+
+}
