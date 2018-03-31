6bef18c0e218de6418678f46e003041aa47ad94b
Meagan Blais <meaganblais@gmail.com>
2018-03-31 12:55:28 -0400

    setting newDna calculations for turned zombie

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -1,7 +1,15 @@
-pragma ^0.4.19;
+pragma solidity ^0.4.19;
 
-import "./ZombieFactory";
+import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId];
+      _targetDna = _targetDna % dnaModulus;
+      uint newDna = (myZombie.dna + _targetDna) / 2;
+      _createZombie("NoName", newDna);
+  }
 
 }
