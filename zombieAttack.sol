d7792676e759ccde985810bf2fd26945b0050b24
Meagan Blais <meaganblais@gmail.com>
2018-04-03 00:01:18 -0400

    setup zombie battle logic, add stats, provide rewards

--- zombieAttack.sol
+++ zombieAttack.sol
@@ -4,9 +4,28 @@ import "./zombieHelper.sol";
 
 contract ZombieBattle is ZombieHelper {
   uint randNonce = 0;
+  uint attackVictoryProbability = 70;
 
+    // function to return random number between 1-100
     function randMod(uint _modulus) internal returns (uint) {
         randNonce++;
         return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+    }
+
+    // create function to initiate zombie battle, determine winner, adjust stats and provide rewards
+    function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+        }
     }
 }
--- zombieFactory.sol
+++ zombieFactory.sol
@@ -17,6 +17,8 @@ contract ZombieFactory is Ownable {
       uint dna;
       uint32 level;
       uint32 readyTime;
+      uint16 winCount;
+      uint16 lossCount;
   }
 
   // create Dynamic (non-fixed) array set to public so other contracts can read
@@ -30,7 +32,7 @@ contract ZombieFactory is Ownable {
   // assign zombie to caller of function and increase owner count
   // execute event to let the app know the function was called
   function _createZombie(string _name, uint _dna) internal {
-      uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+      uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
       zombieToOwner[id] = msg.sender;
       ownerZombieCount[msg.sender]++;
       NewZombie(id, _name, _dna);
