f20d2ae53e185dbd75ce399c6823d1fabeafa8d2
Meagan Blais <meaganblais@gmail.com>
2018-04-03 12:28:54 -0400

    setting up erc721 & refactoring ownerOf function due to duplication

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -23,7 +23,7 @@ contract ZombieFeeding is ZombieFactory {
   // Initialize contract
   KittyInterface kittyContract;
 
-  modifier ownerOf(uint _zombieId) {
+  modifier onlyOwnerOf(uint _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     _;
   }
@@ -46,7 +46,7 @@ contract ZombieFeeding is ZombieFactory {
   // setting up new zombie with mutated dna using zombieOwner who infected them
   // set to internal to avoid misuse and since only needed for feedOnKitty function
   // verify zombie is only eating once a day
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
