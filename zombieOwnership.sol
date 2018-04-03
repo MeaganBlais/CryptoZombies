e4ed3f322e94867ad4a1bcce77ed5ef2755a6df0
Meagan Blais <meaganblais@gmail.com>
2018-04-03 12:30:41 -0400

    adding functions to check zombie owner & total owned

--- zombieOwnership.sol
+++ zombieOwnership.sol
@@ -4,5 +4,15 @@ import "./zombieAttack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+  
+    // check total zombies per owner
+    function balanceOf(address _owner) public view returns (uint256 _balance) {
+        return ownerZombieCount[_owner];
+    }
+
+    // check owner of zombie
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        return zombieToOwner[_tokenId];
+    }
     
 }
